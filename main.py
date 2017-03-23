add96cdeb0c700a4e59e4813d7a9e1b98e48c988
Zhonglin Han <hanzhonglin1990@gmail.com>
2017-03-22 17:11:25 -0700

    changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import librosa
+import os
 import soundfile as sf
 
 from utils import feature as futils
@@ -6,14 +7,19 @@ from utils.dataset import FeatureExtractor
 
 ### extract features from UrbanSound8K
 
-def features_extract_store(folder_name = 'Urban-Sound-Classification',
+def features_extract_store(dir_name = 'Urban-Sound-Classification',
                           sub_folders = [],
                           n_label = 10,
                           meta_data = None,
                           mode = 'train',
                           persist = True):
 
-    data_handler = FeatureExtractor(file_dir = folder_name, num_labels = n_label)
+    data_handler = FeatureExtractor(file_dir = dir_name, num_labels = n_label)
+    for folder in sub_folders:
+        folder_path = dir_name + folder + '/'
+        files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+        print files
+
     if persist :
         pickle.dump(mfcc_pd,open('./data/193_features_'+mode+'.p','wb'))
 
@@ -26,6 +32,10 @@ def features_extract_store(folder_name = 'Urban-Sound-Classification',
 if __name__ == '__main__':
     us_folder = '/home/paperspace/Documents/Notebooks/Urban-Sound-Classification/'
     sub_folder_list = ['fold1', 'fold2', 'fold3', 'fold4', 'fold5', 'fold6', 'fold7', 'fold8', 'fold9', 'fold10']
-    X, sr = librosa.load(us_folder +'UrbanSound8K/audio/fold1/102106-3-0-0.wav')
-    print futils.get_features(X, sr)
+    #X, sr = librosa.load(us_folder +'UrbanSound8K/audio/fold1/102106-3-0-0.wav')
+
+    features_extract_store(dir_name = us_folder + 'UrbanSound8K/audio/',
+                           sub_folders = sub_folder_list[:8],
+                           )
+    #print futils.get_features(X, sr)
     #main()
--- utils/dataset.py
+++ utils/dataset.py
@@ -3,13 +3,11 @@ import librosa
 import numpy as np
 
 class FeatureExtractor(object):
-    def __init__(self, file_dir = None, num_labels = 10):
+    def __init__(self, num_labels = 10):
         self.dataset = []
-        self.dir = file_dir
         self.nlabel = num_labels
 
-    def feature_extractor(self, file_name):
-        full_filepath = self.file_dir + '/' + file_name
+    def feature_extractor(self, full_filepath):
         X, sr = librosa.load(full_filepath)
         mfccs,chroma,mel,contrast,tonnetz = get_feature(X, sr)
         ext_features = np.hstack([mfccs,chroma,mel,contrast,tonnetz])
