b02ee72b65de4db24d1f23a6f49a84620927257d
Zhonglin Han <hanzhonglin1990@gmail.com>
2017-03-23 15:27:01 -0700

    add classifier

--- models/classifer.py
+++ models/classifer.py
@@ -1,11 +1,8 @@
 import tensorflow as tf
 
-
 class AudioNNModel(object):
     """
     an audio classifier with Conv Nerual Network
     """
     def __init__(self, batch_size):
         self.batch_size = batch_size
-
-    
