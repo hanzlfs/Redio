3c1c5463bc4855dd256c78a4d3d5dc97df83873e
Zhonglin Han <hanzhonglin1990@gmail.com>
2017-03-22 17:20:03 -0700

    change main

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import librosa
-import os
+import os, pickle, librosa
 import soundfile as sf
 
 from utils import feature as futils
@@ -17,8 +16,13 @@ def features_extract_store(dir_name = 'Urban-Sound-Classification',
     data_handler = FeatureExtractor(num_labels = n_label)
     for folder in sub_folders:
         folder_path = dir_name + folder + '/'
-        files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
-        print files
+        files_list = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+        for file_name in files_list:
+            try:
+                file_name_path = folder_path + file_name
+                X, sr = librosa.load(file_name_path)
+            except:
+                print(file_name)
 
     if persist :
         pickle.dump(mfcc_pd,open('./data/193_features_'+mode+'.p','wb'))
