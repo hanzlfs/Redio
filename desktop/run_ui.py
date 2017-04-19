af22315f26921cf62e5c1c54088ad0ae65415711
Zhonglin Han <hanzhonglin1990@gmail.com>
2017-04-18 21:40:24 -0700

    run UI

--- desktop/run_ui.py
+++ desktop/run_ui.py
@@ -9,30 +9,31 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'utils'))
 from feature import *
 from predict import *
+import tensorflow as tf
+import torch
+import librosa
+
 
 class ExampleApp(QtGui.QMainWindow, ui_main.Ui_MainWindow):
     def __init__(self, parent=None):
         pyqtgraph.setConfigOption('background', 'w') #before loading widget
         super(ExampleApp, self).__init__(parent)
         self.setupUi(self)
-        self.grFFT.plotItem.showGrid(True, True, 0.7)
         self.grPCM.plotItem.showGrid(True, True, 0.7)
         self.audioAUG, self.updateCount = [], 0
-        self.maxFFT=0
         self.maxPCM=0
         self.ear = SWHear.SWHear(rate=44100,updatesPerSecond=20)
         self.ear.stream_start()
         self.predicted = ['initial']
 
     def accumlate(self, datastream):
-        if self.updateCount == 20 :
-            features = get_feature(self.audioAUG)
-            import pandas as pd
-            tmp = run_prediction(features)
-            print tmp
-            self.audioAUG, self.updateCount = [], 0
+        if self.updateCount ==  20:
+            length = len(self.audioAUG)
+            feature = sound_net_features(self.audioAUG)
+            pred_conf = run_liblinear(feature)
             self.predicted.pop()
-            self.predicted.append('class')
+            self.predicted.append(pred_conf)
+            self.audioAUG, self.updateCount = [], 0
         else:
             if self.updateCount == 0 :
                 self.audioAUG = datastream
@@ -48,18 +49,15 @@ class ExampleApp(QtGui.QMainWindow, ui_main.Ui_MainWindow):
             if pcmMax>self.maxPCM:
                 self.maxPCM=pcmMax
                 self.grPCM.plotItem.setRange(yRange=[-pcmMax,pcmMax])
-            if np.max(self.ear.fft)>self.maxFFT:
-                self.maxFFT=np.max(np.abs(self.ear.fft))
-                #self.grFFT.plotItem.setRange(yRange=[0,self.maxFFT])
-                self.grFFT.plotItem.setRange(yRange=[0,1])
             self.pbLevel.setValue(1000*pcmMax/self.maxPCM)
-            pen=pyqtgraph.mkPen(color='b')
-            self.grPCM.addLegend(offset=(30, 30))
-            className = self.accumlate(self.ear.datax)
-
-            self.grPCM.plot(self.ear.datax,self.ear.data,pen=pen,clear = True, name = className)
-            pen=pyqtgraph.mkPen(color='r')
-            self.grFFT.plot(self.ear.fftx,self.ear.fft/self.maxFFT,pen=pen,clear=True)
+            pen=pyqtgraph.mkPen(color='b', width=2)
+            className = self.accumlate(self.ear.data)
+            self.grPCM.plot(self.ear.datax,self.ear.data,pen=pen,clear = True)#, name = className)
+            font = QtGui.QGraphicsTextItem().font()
+            font.setPointSizeF(22)
+            tb = pyqtgraph.TextItem(className, color=(0, 0, 0), anchor=(-0.5, 0.5), border=None, fill=(255, 255, 255))
+            tb.setFont(font)
+            self.grPCM.addItem(tb)
         QtCore.QTimer.singleShot(1, self.update) # QUICKLY repeat
 
 if __name__=="__main__":
